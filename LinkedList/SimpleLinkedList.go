ef2207325500abe1b5b8256cb60cef02854808a1
조재현 <munkua26@gmail.com>
2020-01-23 13:40:46 +0000

    LinkedList func add

--- LinkedList/SimpleLinkedList.go
+++ LinkedList/SimpleLinkedList.go
@@ -2,21 +2,37 @@ package main
 
 import "fmt"
 
+// List, Node struct 
+
 type List struct {
 
 	Head *Node
 	Tail *Node
 
-}
+}	// complete!!
 
 type Node struct {
 
-	Element int
+	Key interface{}
 	Next *Node
 
-}
+}	// complete!!
 
-func isEmpty(listPointer *List) bool{
+func getList() *List {		// return Empty List
+
+	listPointer := &List{}
+	return listPointer
+	
+}	// complete!!
+
+func getNode() *Node {		// return Empty Node
+
+	nodePointer := &Node{}
+	return nodePointer		// 노드포인터의 주소를 반환
+
+}	// complete!!
+
+func (listPointer *List)isEmpty() bool{	// check this List is Empty
 
 	if listPointer.Head == nil {
 
@@ -30,137 +46,223 @@ func isEmpty(listPointer *List) bool{
 	}
 	
 
-}
+}	// complete!!
 
-func search(value int, listPointer *List) *Node{
+func (listPointer *List)pushToTail(key interface{}) {	// To push a Node after the Tail Node:
 
-	if isEmpty(listPointer) == false {
-
-		findNode := listPointer.Head
-		
-		for {
-			
-			if findNode.Element == value {
-
-				return findNode
-			
-			}
-		
-		}
-		
+	node := &Node{Key : key}
 	
-	}
-	
-}
-
-func getList() *List {
-
-	listPointer := &List{}
-	return listPointer
-	
-}
-
-func getNode() *Node {
-	
-	node := &Node{}
-	return node
-	
-}
-/*
-func insertNode(value int, listPointer *List) {
-
-	nodePointer := getNode(Element : value)
-	
-	if isEmpty(listPointer) == true {
-
-		listPointer.Head = nodePointer
-	
-	} else {
-		
-		for {
-			
-			
-			
-		}
-	
-	}
-
-}
-uncomplited code. make List Search function first. */
-
-func push(value int, listPointer *List) {	// To push a Node after the Tail Node:
-
-	node := &Node{Element : value}
-	
-	if isEmpty(listPointer) == true {
+	if listPointer.isEmpty() == true {
 
 		listPointer.Head = node
+		listPointer.Tail = node
+		node.Next = nil
 	
 	} else {
 
 		listPointer.Tail.Next = node
+		listPointer.Tail = node
+		listPointer.Tail.Next = nil
 	
 	}
 	
-	listPointer.Tail = node
-	
-}
+}	// complete!!
 
-func pushToHead(value int, listPointer *List) {
+func (listPointer *List)pushToHead(key interface{}) {
 
-	node := &Node{Element : value}
+	node := &Node{Key : key}
 	
-	if isEmpty(listPointer) == true {
+	if listPointer.isEmpty() == true {
 
 		listPointer.Head = node
+		listPointer.Tail = node
+		node.Next = nil
 	
-	} else {
-
+	} else {		// 공백리스트가 아닐 경우
+	
 		node.Next = listPointer.Head
 		listPointer.Head = node
-	
+		
 	}
-}
+	
+}	// complete!!
 
-/* func deleteTail(listPointer *List) {
+func (listPointer *List)deleteHead() {
 
-	if listPointer.Head == nil {
-
+	if listPointer.isEmpty() == true {
+		
 		fmt.Println("List is empty!")
 		
 	} else {
 
-		listPointer.Tail = nil
-		&listPointer.Tail
-	
-	}
-
-} uncomplited code. */
-
-func deleteHead(listPointer *List) {
-
-	if isEmpty(listPointer) == false {
-		
 		tempNode := listPointer.Head.Next
 		listPointer.Head.Next = nil
 		listPointer.Head = tempNode
 		
 	}
 	
-}
+}	// complete!!
+
+func (listPointer *List)search(key interface{}) *Node{	// search Node from argument key
+
+	cycleNode := listPointer.Head
+	
+	if listPointer.isEmpty() == true {
+
+		fmt.Println("List is Empty! search fail")
+		
+	} else if listPointer.Head == listPointer.Tail {
+
+		if listPointer.Head.Key == key {
+
+			searchPointer := listPointer.Head
+			return searchPointer
+		
+		}
+	
+	} else {
+		
+		for cycleNode.Next != nil {
+
+			if cycleNode.Key == key {
+
+				return cycleNode
+				
+			} else {
+
+				cycleNode = cycleNode.Next
+				
+			}
+		
+		}
+	
+	}
+	
+	return cycleNode
+	
+}	// complete!!
+
+func (listPointer *List)insertNode(key interface{}, searchPointer *Node) {
+	
+	newNode := &Node{Key : key}
+
+	if listPointer.isEmpty() == true {
+
+		listPointer.pushToHead(key)
+	
+	} else if listPointer.Head == listPointer.Tail {
+
+		listPointer.pushToTail(key)
+	
+	} else {
+
+		if searchPointer.Next == nil {
+
+			listPointer.pushToTail(key)
+		
+		} else if searchPointer == listPointer.Head {
+
+			temp := listPointer.Head.Next
+			listPointer.Head.Next = searchPointer
+			searchPointer.Next = temp
+			
+		} else {
+
+			temp := searchPointer.Next
+			searchPointer.Next = newNode
+			newNode.Next = temp
+		
+		}
+	
+	}
+
+}	// complete!!
+
+
+func (listPointer *List)deleteTail() {
+	
+	if listPointer.Head == nil {		// if list is empty?
+
+		fmt.Println("List is empty!")
+		
+	} else if listPointer.Head.Next == nil {	// if list have only one Node
+	
+		listPointer.Head = nil
+		listPointer.Tail = nil
+		
+	} else {		// list have two or more Node
+		
+		currentNode := listPointer.Head.Next	
+		previousNode := listPointer.Head
+
+		if currentNode.Next == nil {		// if List have only two Node
+
+			previousNode.Next = nil			
+			listPointer.Tail = previousNode
+		
+		} else {				// if List have two or more Node
+
+			for currentNode.Next != nil {	// search until List Tail
+
+				previousNode = currentNode
+				currentNode = currentNode.Next
+		
+			}
+			
+			previousNode.Next = nil
+			listPointer.Tail = previousNode
+
+		}
+		
+	
+	}
+
+}	// complete!!
+
+
+func (listPointer *List)printAllNode() {
+
+	if (listPointer.isEmpty() == true) {
+
+		fmt.Println("List is empty!")
+	
+	} else {
+		
+		cycleNode := listPointer.Head		// cycleNode는 임시적으로 사용할 순환 노드포인터
+		count := 1
+		
+		for (cycleNode.Next != nil) {		// cycleNode의 Next필드가 nil일 때까지
+
+			fmt.Println(count , "Node is = ", cycleNode.Key)		// 가리키는 노드의 Key필드값을 출력
+			cycleNode = cycleNode.Next		// cycleNode를 한칸 앞으로 이동
+			count++
+		
+		}
+		
+		fmt.Println(count , "Node is = ", cycleNode.Key)		// 가리키는 노드의 Key필드값을 출력
+
+
+	}
+
+}	// Complete!!
 
 func main() {
 
-	listPointer := getList()	// listPointer 는 *List 타입
+	listPointer := getList()
+	node := getNode()
 	
-	push(200, listPointer)
-	fmt.Println(listPointer.Head.Element)
+	listPointer.pushToHead(1)
+	listPointer.pushToTail(2)
+	listPointer.pushToHead(3)
 	
-	pushToHead(300, listPointer)
-	fmt.Println(listPointer.Head.Element)
+	node = listPointer.search(2)
+	listPointer.insertNode(4, node)
 	
-	deleteHead(listPointer)
-	fmt.Println(listPointer.Head.Element)
+	listPointer.printAllNode()		// 3124
 	
+	listPointer.deleteHead()		// 124
+	listPointer.deleteTail()		// 12
 	
+	listPointer.printAllNode()		// 12
+
 }
